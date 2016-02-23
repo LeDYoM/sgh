6284743e54332e50136d5efaac89aeb37ef2a4e5
LeDYoM <ledyom@hotmail.com>
2016-02-23 21:48:03 +0100

    Convert function fixed

--- host/lib/core/convops.cpp
+++ host/lib/core/convops.cpp
@@ -15,10 +15,12 @@ namespace lib
 	sf::Transform convert(const draw::Transformation &transform)
 	{
 		const draw::Transformation::TransformationDataType &f{ transform.getMatrix() };
-		return sf::Transform{ f[0], f[1], /*f[2],*/ f[3], 
-			f[4], f[5], /*f[6],*/ f[7], 
-			/*f[8], f[9], f[10], f[11],*/
-			f[12], f[13], /*f[14], */f[15] };
+		sf::Transform temp( f[0], f[4], /*f[8],*/ f[12], 
+							f[1], f[5], /*f[9],*/ f[13], 
+							/*f[2], f[6], f[10], f[14],*/
+							f[3], f[7], /*f[11],*/ f[15] );
+
+		return temp;
 	}
 
 }

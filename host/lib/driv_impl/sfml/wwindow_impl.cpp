fbb83f9714bded8e7abcbe0e0dd3db4bd19aaaa0
unknown <gonzal_i@MU727792.rsint.net>
2016-02-29 11:30:40 +0100

    Dummy error in cmakelists.txt fixed

--- host/lib/driv_impl/sfml/wwindow_impl.cpp
+++ host/lib/driv_impl/sfml/wwindow_impl.cpp
@@ -6,7 +6,7 @@ namespace lib
 	{
 		namespace window
 		{
-			bool lib::drivers::window::SFMLWindow::create(u32 width, u32 height, u32 bpp, const char *title, u32 depth, u32 stencil, u32 antialiasing, u32 major, u32 minor, u32 attributes)
+			bool SFMLWindow::create(u32 width, u32 height, u32 bpp, const char *title, u32 depth, u32 stencil, u32 antialiasing, u32 major, u32 minor, u32 attributes)
 			{
 				width;
 				height;

1e0bef7108d70066c2ad95a0d69e101bc2a92746
LeDYoM <ledyom@hotmail.com>
2016-03-02 21:18:56 +0100

    Config 2.0 with data parser

---
+++ host/lib/core/config.cpp
@@ -0,0 +1,17 @@
+#include "config.hpp"
+#include "log.hpp"
+
+namespace lib
+{
+	namespace core
+	{
+		Config::Config(AppController *const appController)
+			: AppService{ appController }
+		{
+		}
+
+		void Config::loadFile(const std::string &file)
+		{
+		}
+	}
+}
---
+++ host/lib/core/config.hpp
@@ -0,0 +1,25 @@
+#ifndef __LIB_CONFIGURATION_HPP__
+#define __LIB_CONFIGURATION_HPP__
+
+#include <lib/include/types.hpp>
+#include "appservice.hpp"
+#include <string>
+
+namespace lib
+{
+	namespace core
+	{
+		class Config : public AppService
+		{
+		public:
+			Config(AppController *const appController);
+			virtual ~Config() {}
+
+		private:
+			void loadFile(const std::string &file);
+			const std::string currentFile;
+		};
+	}
+}
+
+#endif

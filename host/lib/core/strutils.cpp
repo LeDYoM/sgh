abffea84596a5d820e39a63e8812eabd6aa4b25f
unknown <gonzal_i@MU727792.rsint.net>
2016-03-08 09:49:42 +0100

    Utilities for strings

---
+++ host/lib/core/strutils.cpp
@@ -0,0 +1,47 @@
+#include "strutils.hpp"
+#include <cctype>
+
+namespace lib
+{
+	bool starts_with(const std::string &str, const std::string &what)
+	{
+		return str.find_first_of(what) == 0;
+	}
+
+	void ltrim(std::string &str)
+	{
+		while (!str.empty() && std::isspace(str[0]))
+			str = str.substr(1);
+	}
+
+	void rtrim(std::string &str)
+	{
+		while (!str.empty() && std::isspace(str[str.size() - 1]))
+			str = str.substr(0, str.size() - 2);
+	}
+
+	void trim(std::string &str)
+	{
+		ltrim(str);
+		rtrim(str);
+	}
+
+	void leftFrom(std::string &str, const std::string &what)
+	{
+		auto pos = str.find_first_of(what);
+		if (pos != std::string::npos)
+		{
+			str = str.substr(0, pos);
+		}
+	}
+
+	void rightFrom(std::string &str, const std::string &what)
+	{
+		auto pos = str.find_first_of(what);
+		if (pos != std::string::npos)
+		{
+			str = str.substr(pos+what.size());
+		}
+	}
+
+}
---
+++ host/lib/core/strutils.hpp
@@ -0,0 +1,16 @@
+#ifndef LIB_STRUTILS_HPP
+#define LIB_STRUTILS_HPP
+
+#include <string>
+
+namespace lib
+{
+	bool starts_with(const std::string &str, const std::string &what);
+	void ltrim(std::string &str);
+	void rtrim(std::string &str);
+	void trim(std::string &str);
+	void leftFrom(std::string &str, const std::string &what);
+	void rightFrom(std::string &str, const std::string &what);
+}
+
+#endif

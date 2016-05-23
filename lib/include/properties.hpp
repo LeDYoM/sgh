94aa42e10b3b2d13c6ad08a410316a2643d7c229
LeDYoM <ledyom@hotmail.com>
2016-05-23 20:15:28 +0200

    wip properties

--- lib/include/properties.hpp
+++ lib/include/properties.hpp
@@ -5,25 +5,18 @@
 
 namespace lib
 {
-	template <typename T,class ACCESSOR>
+	template <typename T>
 	class Property
 	{
 	public:
-		const T &get() const { return m_value; }
-	private:
 		Property() : m_value{} {}
 		Property(Property&&r) : m_value{ std::move(p); } {}
 		explicit Property(const T&iv) :m_value{ iv } {}
 		explicit Property(const T&&iv) :m_value{ std::move(iv); } {}
 		explicit Property(const Property &p) : m_value{ p } {}
 		T m_value;
-		void set(const T&nv) { m_value = nv; }
 		Property &operator=(const T&nv) { m_value = nv; return *this; }
 		Property &operator=(T&&nv) { m_value = nv; return *this; }
-
-		friend ACCESSOR;
 	};
-
-	template <typename T, class ACCESSOR> using PropertySptr = Property<sptr<T>,ACCESSOR>;
 }
 #endif

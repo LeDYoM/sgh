01434273def7ca1f621fe8fe3e3c8e82c4f5a243
LeDYoM <ledyom@hotmail.com>
2016-05-24 22:33:19 +0200

    Nightly

--- lib/include/properties.hpp
+++ lib/include/properties.hpp
@@ -15,21 +15,9 @@ namespace lib
 	class Property
 	{
 	public:
-		Property() : m_value{} {}
-		explicit Property(const T&iv) :m_value{ iv } {}
-		Property &operator=(const T&nv) { m_value = nv; return *this; }
-		operator T() const { return m_value; }
-	private:
-		T m_value;
-	};
-
-	template <typename T>
-	class PropertyWithMethods
-	{
-	public:
 //		using Getter = std::function<const draw::Color()>;
 //		using Setter = std::function<void(const draw::Color)>;
-		explicit PropertyWithMethods(const T&iv) : m_value{ iv } {}
+		explicit Property(const T&iv) : m_value{ iv } {}
 //		explicit PropertyWithMethods(const draw::Color&iv, Getter getter, Setter setter) :m_value{ iv }, m_getter{ getter }, m_setter{ setter } {}
 //		PropertyWithMethods &operator=(const draw::Color&nv) { m_setter(nv); return *this; }
 		operator const T&() const { return m_value; }

39e513e466ebdfca86f7fec0e520b3e8ff903406
Ismael González Burgos <igonzalez@mrr-consulting.com>
2016-06-22 18:15:45 +0200

    wip

--- lib/core/obj.cpp
+++ lib/core/obj.cpp
@@ -7,14 +7,9 @@ namespace lib
 		: m_app{ nullptr } {}
 
 	Object::Object(Object *other)
-		: m_app{ other->m_app }
-	{
-		if (other) {
-			m_app = other->m_app;
-		}
-	}
+		: m_app{ other ? other->m_app : nullptr } { }
 
-	Object::Object(core::AppController *app)
+	Object::Object(core::AppController *app) 
 		: m_app{ app } {}
 
 	void Object::setProvider(core::AppController *app)

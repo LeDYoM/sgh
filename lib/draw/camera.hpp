6e28c7eef8af379abee1180e5eb34cec4a50d1c5
LeDYoM <ledyom@hotmail.com>
2016-06-29 23:34:17 +0200

    wip

--- lib/draw/camera.hpp
+++ lib/draw/camera.hpp
@@ -7,20 +7,40 @@ namespace lib
 {
 	namespace draw
 	{
-		class CameraPriv;
-
 		class Camera
 		{
 		public:
-			explicit Camera(const Rectf32 &rectangle);
-			inline const Rectf32 &target() const { return m_target; }
-			void setSize(const vector2df &size);
-			~Camera();
+			Camera();
+			explicit Camera(const FloatRect& rectangle);
+			Camera(const Vector2f& center, const Vector2f& size);
+			void setCenter(float x, float y);
+			void setCenter(const Vector2f& center);
+			void setSize(float width, float height);
+			void setSize(const Vector2f& size);
+			void setRotation(float angle);
+			void setViewport(const FloatRect& viewport);
+			void reset(const FloatRect& rectangle);
+			const Vector2f& getCenter() const;
+			const Vector2f& getSize() const;
+			float getRotation() const;
+			const FloatRect& getViewport() const;
+			void move(float offsetX, float offsetY);
+			void move(const Vector2f& offset);
+			void rotate(float angle);
+			void zoom(float factor);
+			const Transform& getTransform() const;
+			const Transform& getInverseTransform() const;
 		private:
-			uptr<CameraPriv> m_priv;
-			Rectf32 m_target;
-		};
-	}
+			Vector2f          m_center;
+			Vector2f          m_size;
+			float             m_rotation;
+			FloatRect         m_viewport;
+			mutable Transform m_transform;
+			mutable Transform m_inverseTransform;
+			mutable bool      m_transformUpdated;
+			mutable bool      m_invTransformUpdated;
+		}
+	};
 }
 
 #endif

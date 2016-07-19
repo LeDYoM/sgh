1f01630c3802b0a9e43f3a69eb1a3b004c8c61fa
LeDYoM <ledyom@hotmail.com>
2016-07-19 13:19:15 +0200

    Small optimizations

--- host/keymapping.cpp
+++ host/keymapping.cpp
@@ -2,32 +2,36 @@
 #include <lib/core/configuration.hpp>
 #include <lib/core/appcontroller.hpp>
 
+using namespace lib;
+
 namespace zoper
 {
-	KeyMapping::KeyMapping(lib::Object *const obj)
-		: lib::Object{ obj } {}
+	KeyMapping::KeyMapping(Object *const obj)
+		: Object{ obj } {}
 
 	KeyMapping::~KeyMapping() {}
 
 	void KeyMapping::init()
 	{
-		std::array<lib::KeyCode, TotalKeys> defaults{ lib::KeyCode::Left, lib::KeyCode::Right, lib::KeyCode::Up, lib::KeyCode::Down, lib::KeyCode::Space, lib::KeyCode::Escape };
+		auto configuration(service<Configuration>());
+		std::array<KeyCode, TotalKeys> defaults = { KeyCode::Left, KeyCode::Right, KeyCode::Up, KeyCode::Down, KeyCode::Space, KeyCode::Escape };
 
 		for (auto i = 0u; i < Direction::Total; ++i) {
-			_keys[i] = static_cast<lib::KeyCode>(service<lib::Configuration>()->get("Keyboard/key" + std::to_string(i), lib::DataValue{ static_cast<lib::s32>(defaults[i]) }).gets32());
+			_keys[i] = static_cast<KeyCode>(configuration->get("Keyboard/key" + std::to_string(i), DataValue{ static_cast<s32>(defaults[i]) }).gets32());
 		}
 
-		_keys[Direction::Total] = static_cast<lib::KeyCode>(service<lib::Configuration>()->get("Keyboard/key_launch" + std::to_string(Direction::Total), lib::DataValue{ static_cast<lib::s32>(defaults[Direction::Total]) }).gets32());
-		_keys[Direction::Total+1] = static_cast<lib::KeyCode>(service<lib::Configuration>()->get("Keyboard/key_pause" + std::to_string(Direction::Total + 1), lib::DataValue{ static_cast<lib::s32>(defaults[Direction::Total + 1]) }).gets32());
+		_keys[Direction::Total] = static_cast<KeyCode>(configuration->get("Keyboard/key_launch" + std::to_string(Direction::Total), DataValue{ static_cast<s32>(defaults[Direction::Total]) }).gets32());
+		_keys[Direction::Total + 1] = static_cast<KeyCode>(configuration->get("Keyboard/key_pause" + std::to_string(Direction::Total + 1), DataValue{ static_cast<s32>(defaults[Direction::Total + 1]) }).gets32());
 	}
 
-	lib::KeyCode KeyMapping::getKey(Direction d) const
+	KeyCode KeyMapping::getKey(Direction d) const
 	{
 		return _keys[d];
 	}
-	Direction KeyMapping::getDirectionFromKey(lib::KeyCode k) const
+
+	Direction KeyMapping::getDirectionFromKey(KeyCode k) const
 	{
-		for (lib::u8 i = 0; i < Direction::Total; ++i)
+		for (u8 i = 0; i < Direction::Total; ++i)
 		{
 			if (_keys[i] == k)
 			{
@@ -37,31 +41,31 @@ namespace zoper
 		return Direction::Invalid;
 	}
 
-	lib::KeyCode KeyMapping::getLaunchKey() const
+	KeyCode KeyMapping::getLaunchKey() const
 	{
 		return _keys[Direction::Total];
 	}
 
-	bool KeyMapping::isLaunchKey(const lib::KeyCode key) const
+	bool KeyMapping::isLaunchKey(const KeyCode key) const
 	{
 		return key == getLaunchKey();
 	}
 
-	lib::KeyCode KeyMapping::getPauseKey() const
+	KeyCode KeyMapping::getPauseKey() const
 	{
 		return _keys[Direction::Total+1];
 	}
 
-	bool KeyMapping::isPauseKey(const lib::KeyCode key) const
+	bool KeyMapping::isPauseKey(const KeyCode key) const
 	{
 		return key == getPauseKey();
 	}
 
-	bool KeyMapping::setKey(lib::u32 index, lib::KeyCode key)
+	bool KeyMapping::setKey(u32 index, KeyCode key)
 	{
 		__ASSERT(index < TotalKeys, "Invalid index");
 
-		for (lib::u32 i = 0u; i < index; ++i)
+		for (u32 i = 0u; i < index; ++i)
 		{
 			if (_keys[i] == key)
 			{

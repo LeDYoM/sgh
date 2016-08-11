7b26135305533dde00276fb874d92fc63d312ee3
LeDYoM <ledyom@hotmail.com>
2016-08-11 09:28:46 +0200

    StringMap deinlined

--- lib/core/strmap.cpp
+++ lib/core/strmap.cpp
@@ -3,42 +3,56 @@
 
 #include <lib/include/types.hpp>
 #include <map>
+#include "strmap.hpp"
 
 namespace lib
 {
 	namespace core
 	{
-		class StringMap
+		using DataTypeInternal = std::map<StringMap::Index, StringMap::Value>;
+		using Node = std::pair<StringMap::Index, StringMap::Value>;
+
+		struct StringMapPrivate
 		{
-		public:
-			using Index = const str;
-			using Value = str;
-			using DataTypeInternal = std::map<Index, Value>;
-			using Node = std::pair<Index,Value>;
-
-			StringMap() {}
-			virtual ~StringMap() {}
-
-			inline bool exists(Index index) const { return m_data.find(index) != m_data.end(); }
-			inline bool add(Index index, const Value value)
-			{
-				if (!exists(index)) {
-					m_data[index] = value;
-					return true;
-				}
-				return false;
-			}
-			inline bool update(Index index, const Value value)
-			{
-				if (exists(index)) {
-					m_data[index] = value;
-					return true;
-				}
-				return false;
-			}
-		private:
 			DataTypeInternal m_data;
+
 		};
+
+		StringMap::StringMap()
+			: m_private(new StringMapPrivate)
+		{
+
+		}
+
+		StringMap::~StringMap()
+		{
+			delete m_private;
+		}
+
+		bool StringMap::exists(Index index) const
+		{
+			return m_private->m_data.find(index) != m_private->m_data.end();
+		}
+
+		bool StringMap::add(Index index, const Value value)
+		{
+			if (!exists(index)) {
+				m_private->m_data[index] = value;
+				return true;
+			}
+			return false;
+		}
+
+		bool StringMap::update(Index index, const Value value)
+		{
+			if (exists(index)) {
+				m_private->m_data[index] = value;
+				return true;
+			}
+			return false;
+		}
+
+
 	}
 }
 
--- lib/core/strmap.hpp
+++ lib/core/strmap.hpp
@@ -1,5 +1,5 @@
-#ifndef __LIB_CORE_TYPES_HPP__
-#define __LIB_CORE_TYPES_HPP__
+#ifndef __LIB_CORE_STRMAP_HPP__
+#define __LIB_CORE_STRMAP_HPP__
 
 #include <lib/include/types.hpp>
 #include <map>
@@ -13,31 +13,15 @@ namespace lib
 		public:
 			using Index = const str;
 			using Value = str;
-			using DataTypeInternal = std::map<Index, Value>;
-			using Node = std::pair<Index,Value>;
 
-			StringMap() {}
-			virtual ~StringMap() {}
+			StringMap();
+			virtual ~StringMap();
 
-			inline bool exists(Index index) const { return m_data.find(index) != m_data.end(); }
-			inline bool add(Index index, const Value value)
-			{
-				if (!exists(index)) {
-					m_data[index] = value;
-					return true;
-				}
-				return false;
-			}
-			inline bool update(Index index, const Value value)
-			{
-				if (exists(index)) {
-					m_data[index] = value;
-					return true;
-				}
-				return false;
-			}
+			bool exists(Index index) const;
+			bool add(Index index, const Value value);
+			bool update(Index index, const Value value);
 		private:
-			DataTypeInternal m_data;
+			struct StringMapPrivate *m_private;
 		};
 	}
 }

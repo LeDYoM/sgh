58aa77f710974aa81049c0baf8b9365ebff5579a
LeDYoM <ledyom@hotmail.com>
2016-08-18 00:34:22 +0200

    Nightly

--- moduler/include/moduler.hpp
+++ moduler/include/moduler.hpp
@@ -1,13 +1,12 @@
-file (GLOB MODULER_SOURCES "*.cpp")
-file (GLOB MODULER_HEADERS "*.hpp")
- 
-file (GLOB MODULER_INCLUDES "include/*.hpp")
- 
-source_group("Sources" FILES ${MODULER_SOURCES})
-source_group("Headers" FILES ${MODULER_HEADERS})
-source_group("Includes" FILES ${MODULER_INCLUDES})
- 
-include_directories (.)
- 
-add_library(moduler SHARED ${MODULER_SOURCES} ${MODULER_HEADERS} ${MODULER_INCLUDES})
-install (TARGETS moduler RUNTIME DESTINATION ${PROJECT_BINARY_DIR}/bin)
+#ifndef MODULER_INCLUDE_HPP
+#define MODULER_INCLUDE_HPP
+
+class Moduler
+{
+private:
+	Moduler();
+	~Moduler();
+};
+
+
+#endif
--- moduler/moduler.cpp
+++ moduler/moduler.cpp
@@ -1,13 +1,11 @@
-file (GLOB MODULER_SOURCES "*.cpp")
-file (GLOB MODULER_HEADERS "*.hpp")
- 
-file (GLOB MODULER_INCLUDES "include/*.hpp")
- 
-source_group("Sources" FILES ${MODULER_SOURCES})
-source_group("Headers" FILES ${MODULER_HEADERS})
-source_group("Includes" FILES ${MODULER_INCLUDES})
- 
-include_directories (.)
- 
-add_library(moduler SHARED ${MODULER_SOURCES} ${MODULER_HEADERS} ${MODULER_INCLUDES})
-install (TARGETS moduler RUNTIME DESTINATION ${PROJECT_BINARY_DIR}/bin)
+#include "include/moduler.hpp"
+
+static Moduler *instance{ nullptr };
+
+Moduler::Moduler()
+{
+}
+
+Moduler::~Moduler()
+{
+}

4e4e04bac6f68f8cdc7787a022a1a041b0b03385
LeDYoM <ledyom@hotmail.com>
2016-08-25 00:17:42 +0200

    Keeping track of loaded methods

--- loader/loadedInstance.cpp
+++ loader/loadedInstance.cpp
@@ -1,25 +1,39 @@
-#include "loadedInstance.hpp"
+#include "loadedinstance.hpp"
+#include <map>
+#include <string>
 
-#ifdef _WIN32
-#include <windows.h>
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+	inline void *getMethod(void *handle, const char *methodName)
+	{
+		return static_cast<void*>(GetProcAddress(static_cast<HMODULE>(handle), methodName));
+	}
+
+	inline void *loadSharedObject(const char *fileName)
+	{
+		return static_cast<void*>(LoadLibrary(fileName));
+	}
 #endif
 
 namespace loader
 {
+	using namespace std;
+
 	class LoadedInstancePrivate
 	{
 	public:
 		LoadedInstancePrivate() {}
-		~LoadedInstancePrivate() {}
-#ifdef _WIN32
-		HINSTANCE m_dllHandle;
-#endif
+		~LoadedInstancePrivate() 
+		{
+			m_methods.clear();
+		}
+
+		void *m_sharedFileHandle;
+		map<string, void*> m_methods;
 	};
 
-	LoadedInstance::LoadedInstance() : m_private{ new LoadedInstancePrivate }
-	{
-
-	}
+	LoadedInstance::LoadedInstance() : m_private{ new LoadedInstancePrivate } {}
 
 	LoadedInstance::~LoadedInstance()
 	{
@@ -28,26 +42,33 @@ namespace loader
 
 	bool LoadedInstance::load(const char * fileName)
 	{
-#ifdef _WIN32
-		m_private->m_dllHandle = LoadLibrary(fileName);
-#endif
+		m_private->m_sharedFileHandle = loadSharedObject(fileName);
 		return loaded();
 	}
 
 	void *LoadedInstance::loadMethod(const char *methodName)
 	{
-		if (loaded())
-		{
-#ifdef _WIN32
-			return static_cast<void*>(GetProcAddress(m_private->m_dllHandle,methodName));
-#endif
+		if (loaded()) {
+			auto node(m_private->m_methods.find(methodName));
+			if (node == m_private->m_methods.end()) {
+				auto methodAddress(getMethod(m_private->m_sharedFileHandle, methodName));
+
+				// Add the result of getMethod even if is nullptr to avoid
+				// trying to load it more times
+				m_private->m_methods[methodName] = methodAddress;
+				return methodAddress;
+			}
+			else {
+				// The method is already on the map, return it
+				return node->second;
+			}
 		}
 		return nullptr;
 	}
 
 	bool LoadedInstance::loaded() const
 	{
-		return m_private->m_dllHandle != nullptr;
+		return m_private->m_sharedFileHandle != nullptr;
 	}
 
 	bool LoadedInstance::unload()
@@ -55,9 +76,9 @@ namespace loader
 		if (loaded())
 		{
 #ifdef _WIN32
-			FreeLibrary(m_private->m_dllHandle);
+			FreeLibrary(static_cast<HMODULE>(m_private->m_sharedFileHandle));
 #endif
-			m_private->m_dllHandle = nullptr;
+			m_private->m_sharedFileHandle = nullptr;
 			return true;
 		}
 		return false;
@@ -65,7 +86,7 @@ namespace loader
 
 	void * LoadedInstance::loadedData() const
 	{
-		return static_cast<void*>(m_private->m_dllHandle);
+		return m_private->m_sharedFileHandle;
 	}
 
 }

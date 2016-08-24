724aee6c9debac554f920243683f95d250786a07
LeDYoM <ledyom@hotmail.com>
2016-08-24 12:56:17 +0200

    Using new logger features

--- moduler/moduler.cpp
+++ moduler/moduler.cpp
@@ -1,6 +1,7 @@
 #include "include/moduler.hpp"
 #include <loader/include/loader.hpp>
 #include <logger/include/logger.hpp>
+#include <sstream>
 
 namespace moduler
 {
@@ -29,7 +30,7 @@ namespace moduler
 
 	IModule *Moduler::loadModule(const char * fileName)
 	{
-		LOG_DEBUG("Going to open ")
+		LOG_DEBUG_STR("Going to open " << fileName);
 		auto *moduleData(m_private->loaderInstance->loadModule(fileName));
 		if (moduleData) {
 			IModule *loadedModule = reinterpret_cast<IModule*>(moduleData);

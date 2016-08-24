17ed35448da12b2e6154bb61f9cead1d053123e3
LeDYoM <ledyom@hotmail.com>
2016-08-24 15:56:26 +0200

    Creators method interfaces changed

--- moduler/include/ibasicinterface.hpp
+++ moduler/include/ibasicinterface.hpp
@@ -3,7 +3,7 @@
 
 #ifndef MODULER_EXPORTS
 	#ifdef _WIN32
-		#define EXPORT_API   __declspec( dllexport ) 
+		#define EXPORT_API   extern "C" __declspec( dllexport ) 
 	#else
 		#define EXPORT_API
 	#endif

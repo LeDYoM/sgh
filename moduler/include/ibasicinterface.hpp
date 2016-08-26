bb1c951fb96e7ae91cce3ba0348817fc0e092ec5
LeDYoM <ledyom@hotmail.com>
2016-08-26 12:41:27 +0200

    Moved definitions to module to a new file

--- moduler/include/ibasicinterface.hpp
+++ moduler/include/ibasicinterface.hpp
@@ -1,16 +1,6 @@
 #ifndef MODULER_BASIC_INTERFACE_INCLUDE_HPP
 #define MODULER_BASIC_INTERFACE_INCLUDE_HPP
 
-#ifndef MODULER_EXPORTS
-	#ifdef _WIN32
-		#define EXPORT_API   extern "C" __declspec( dllexport ) 
-	#else
-		#define EXPORT_API
-	#endif
-#else
-	#define EXPORT_API
-#endif
-
 namespace moduler
 {
 	class IBasicInterface
--- moduler/include/imoduleexport.hpp
+++ moduler/include/imoduleexport.hpp
@@ -3,38 +3,48 @@
 
 #include "imodule.hpp"
 
-#define CREATE_MODULE_FUNC_NAME	createModule
-#define DELETE_MODULE_FUNC_NAME	deleteModule
+#define CREATE_MODULE_FUNC_NAME		createModule
+#define GET_MODULE_FUNC_NAME		getModule
+#define DELETE_MODULE_FUNC_NAME		deleteModule
 
 #define CREATE_MODULE_FUNC_NAME_STR	"createModule"
 #define GET_MODULE_FUNC_NAME_STR	"getModule"
 #define DELETE_MODULE_FUNC_NAME_STR "deleteModule"
 
-#define DECLARE_MODULE_CREATION     \
-	EXPORT_API bool createModule();     \
-	EXPORT_API moduler::IModule* getModule();     \
-    EXPORT_API bool deleteModule();
+#if !defined MODULER_EXPORTS && defined _MSC_VER
+	#define EXPORT_API   extern "C" __declspec( dllexport ) 
+#else
+	#define EXPORT_API
+#endif
 
-// Note: Use this define only in a not exported cpp/c file
-#define DEFINE_MODULE_CREATION(className)     \
-	static className *m_instance##className{nullptr};	\
-	bool CREATE_MODULE_FUNC_NAME()	\
-	{	\
-		if (!m_instance##className)	{	\
-			m_instance##className = new className;	\
-			return true;	\
+#ifndef MODULE_EXPORTS
+
+#define DECLARE_MODULE_CREATION		\
+	EXPORT_API bool CREATE_MODULE_FUNC_NAME();	\
+	EXPORT_API moduler::IModule* GET_MODULE_FUNC_NAME();	\
+	EXPORT_API bool DELETE_MODULE_FUNC_NAME();
+
+	// Note: Use this define only in a not exported cpp/c file
+	#define DEFINE_MODULE_CREATION(className)     \
+		static className *m_instance##className{nullptr};	\
+		bool CREATE_MODULE_FUNC_NAME()	\
+		{	\
+			if (!m_instance##className)	{	\
+				m_instance##className = new className;	\
+				return true;	\
+			}	\
+			return false;	\
 		}	\
-		return false;	\
-	}	\
-	moduler::IModule* getModule() {return m_instance##className; }	\
-    bool DELETE_MODULE_FUNC_NAME()	\
-	{	\
-		if (m_instance##className) {	\
-			delete m_instance##className;	\
-			m_instance##className = nullptr;	\
-			return true;	\
-		}	\
-		return false;	\
-	}
+		moduler::IModule* getModule() {return m_instance##className; }	\
+		bool DELETE_MODULE_FUNC_NAME()	\
+		{	\
+			if (m_instance##className) {	\
+				delete m_instance##className;	\
+				m_instance##className = nullptr;	\
+				return true;	\
+			}	\
+			return false;	\
+		}
+#endif
 
 #endif

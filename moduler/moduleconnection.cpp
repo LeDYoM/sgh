b0f69fcb90a718d4e66f793cbb744bda8091781c
LeDYoM <ledyom@hotmail.com>
2016-09-07 09:05:01 +0200

    Added ModuleConnection class

---
+++ moduler/moduleconnection.cpp
@@ -0,0 +1,5 @@
+#include "moduleconnection.hpp"
+
+namespace moduler
+{
+}
---
+++ moduler/moduleconnection.hpp
@@ -0,0 +1,17 @@
+#ifndef MODULER_MODULECONNECTION_HPP
+#define MODULER_MODULECONNECTION_HPP
+
+namespace moduler
+{
+	class ModuleConnection
+	{
+	public:
+		ModuleConnection() {}
+		virtual ~ModuleConnection() {}
+
+	private:
+
+	};
+}
+
+#endif

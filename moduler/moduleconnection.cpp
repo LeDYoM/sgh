2563bc4f82fa3f8f76d1d288fcde8708d40d87a1
LeDYoM <ledyom@hotmail.com>
2016-09-08 14:39:25 +0200

    wip

--- moduler/moduleconnection.cpp
+++ moduler/moduleconnection.cpp
@@ -1,5 +1,44 @@
 #include "moduleconnection.hpp"
+#include "modulehandle.hpp"
+#include <algorithm>
 
 namespace moduler
 {
+	namespace pcontainer
+	{
+		template <typename Container, typename Content>
+		std::pair<bool,typename Container::const_iterator> find(const Container &container, const Content *const element)
+		{
+			const auto iterator(std::find_if(container.cbegin(), container.cend(), [element](const Content *node) {
+				return *node == *element;
+			}));
+			return{ iterator != container.cend(),iterator };
+		}
+
+		template <typename Container, typename Content>
+		bool add_if_not_exists(const Container &container, const Content *const &&element)
+		{
+			const auto result(find(container, element));
+			if (!result.first) {
+				container.emplace_back(element);
+				return true;
+			}
+			return false;
+		}
+
+	}
+
+	ModuleConnection::~ModuleConnection()
+	{
+	}
+
+	bool ModuleConnection::hasModule(const ModuleHandle *const requiredModule) const
+	{
+		return pcontainer::find(m_dest, requiredModule).first;
+	}
+
+	bool ModuleConnection::addRequiredModule(ModuleHandle * requiredModule)
+	{
+		return pcontainer::add_if_not_exists(m_dest, std::move(requiredModule));
+	}
 }
--- moduler/moduleconnection.hpp
+++ moduler/moduleconnection.hpp
@@ -1,16 +1,24 @@
 #ifndef MODULER_MODULECONNECTION_HPP
 #define MODULER_MODULECONNECTION_HPP
 
+#include <list>
+
 namespace moduler
 {
+	struct ModuleHandle;
+
 	class ModuleConnection
 	{
 	public:
-		ModuleConnection() {}
-		virtual ~ModuleConnection() {}
+		explicit ModuleConnection(ModuleHandle *source) noexcept : m_source{ source } {}
+		~ModuleConnection();
+
+		bool hasModule(const ModuleHandle *const requiredModule) const;
+		bool addRequiredModule(ModuleHandle *requiredModule);
 
 	private:
-
+		ModuleHandle *m_source;
+		std::list<ModuleHandle *> m_dest;
 	};
 }
 

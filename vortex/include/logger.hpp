ceaaf2b177c3930151aed54cdf79d7bd3ed4caac
LeDYoM <ledyom@hotmail.com>
2016-09-21 14:58:46 +0200

    Logger wip

--- vortex/include/logger.hpp
+++ vortex/include/logger.hpp
@@ -2,17 +2,40 @@
 #define VTX_LOGGER_INCLUDE_HPP
 
 #include "comp_config.hpp"
+#include "common_def.hpp"
 #include "singleton.hpp"
 
 namespace vtx
 {
+	class VORTEX_API Log
+	{
+	public:
+		struct EndLine_t {};
+		static constexpr const EndLine_t endLine() { return EndLine_t{}; }
+		Log &operator<<(const char *const message);
+		Log &operator<<(int message);
+		Log &operator<<(const struct EndLine_t);
+
+	private:
+		Log() {}
+		friend class Logger;
+	};
+
 	class VORTEX_API Logger : public Singleton<Logger>
 	{
+	public:
+		Log &debug();
+		Log &info();
+		Log &warning();
+		Log &error();
 	protected:
 		Logger();
 		~Logger();
 		friend class Singleton<Logger>;
 		friend class Vortex;
+
+		DECLARE_PRIVATE_MPRIVATE_PIMPL(Logger)
+
 	};
 }
 
--- vortex/logger.cpp
+++ vortex/logger.cpp
@@ -1,14 +1,67 @@
 #include "include/logger.hpp"
 
+#include "common_def_priv.hpp"
+
+#include <sstream>
 
 namespace vtx
 {
-	Logger::Logger()
+	struct PRIVATE_STRUCT_NAME(Logger)
+	{
+		Log m_debug;
+		Log m_info;
+		Log m_warning;
+		Log m_error;
+	};
+
+	std::ostringstream m_buffer;
+
+
+	Log & Log::operator<<(const char * const message)
+	{
+		m_buffer << message;
+		return *this;
+	}
+
+	Log & Log::operator<<(int message)
+	{
+		m_buffer << message;
+		return *this;
+	}
+
+	Log & Log::operator<<(const EndLine_t)
+	{
+		m_buffer << std::endl;
+		return *this;
+	}
+
+	Log & Logger::debug()
+	{
+		return m_private->m_debug;
+	}
+
+	Log & Logger::info()
+	{
+		return m_private->m_info;
+	}
+
+	Log & Logger::warning()
+	{
+		return m_private->m_warning;
+	}
+
+	Log & Logger::error()
+	{
+		return m_private->m_error;
+	}
+
+	Logger::Logger() : m_private{ new PRIVATE_STRUCT_NAME(Logger) }
 	{
 	}
 
 
 	Logger::~Logger()
 	{
+		DELETE_PRIVATE_MPRIVATE_PIMPL(Logger)
 	}
 }
